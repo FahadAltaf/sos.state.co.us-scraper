fd816cbb7ca4062565c0c050573879fc5725e8ec
Fahad Altaf <fahadaltaf10SE49@hotmail.com>
2023-05-15 14:31:28 -0700

    chnages

--- sos.state.co.us-scraper/Program.cs
+++ sos.state.co.us-scraper/Program.cs
@@ -10,21 +10,21 @@ using Dapper;
 List<ColoradoDataset> entries = new List<ColoradoDataset>();
 using (var connection = new SqlConnection(Environment.GetEnvironmentVariable("WebConnectionString")))
 {
-    entries = connection.Query<ColoradoDataset>("Select * from ColoradoDatasets where agentFirstName is NULL").ToList();
+    entries = connection.Query<ColoradoDataset>("Select * from ColoradoDatasets where agentFirstName is NULL and agentCompany is NULL order by entityid desc").ToList();
 }
 
-int pages = (entries.Count + 10 - 1) / 10;
+int pages = (entries.Count + 5 - 1) / 5;
 List<Task> tasks = new List<Task>();
 for (int count = 1; count <= pages; ++count)
 {
     int index = count - 1;
-    var data = entries.Skip(index * 10).Take(10).ToList();
+    var data = entries.Skip(index * 5).Take(5).ToList();
 
     Task newTask = Task.Factory.StartNew(() => { FetchRegisteredAgentDetails(data).Wait(); });
 
     tasks.Add(newTask);
 
-    if (count % 1 == 0 || count == pages)
+    if (count % 10 == 0 || count == pages)
     {
         foreach (Task task in tasks)
         {
@@ -46,11 +46,16 @@ async Task FetchRegisteredAgentDetails(List<ColoradoDataset> data)
     await browserFetcher.DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
     var browser = await extra.LaunchAsync(new LaunchOptions()
     {
-        Headless = false,
-        DefaultViewport = null
+        DefaultViewport = null,
+        Headless = true,
+        Args = new string[] { "--proxy-server=" + Environment.GetEnvironmentVariable("DCProxyServer", EnvironmentVariableTarget.Process) }
     });
-
     var page = await browser.NewPageAsync();
+    await page.AuthenticateAsync(new Credentials
+    {
+        Username = Environment.GetEnvironmentVariable("ProxyUsername", EnvironmentVariableTarget.Process),
+        Password = Environment.GetEnvironmentVariable("ProxyPassword", EnvironmentVariableTarget.Process)
+    });
     using (var connection = new SqlConnection(Environment.GetEnvironmentVariable("WebConnectionString")))
     {
         foreach (var entry in data)
@@ -58,7 +63,7 @@ async Task FetchRegisteredAgentDetails(List<ColoradoDataset> data)
             try
             {
                 await page.GoToAsync($"https://www.sos.state.co.us/biz/BusinessEntityDetail.do?quitButtonDestination=BusinessEntityCriteriaExt&fileId={entry.entityid}&masterFileId=");
-                await page.WaitForTimeoutAsync(3000);
+                await page.WaitForTimeoutAsync(5000);
 
                 var doc = new HtmlDocument();
                 doc.LoadHtml(await page.GetContentAsync());
@@ -66,7 +71,7 @@ async Task FetchRegisteredAgentDetails(List<ColoradoDataset> data)
                 var regNameNode = doc.DocumentNode.SelectSingleNode("//*[@id=\"application\"]/table/tbody/tr/td[2]/table/tbody/tr[3]/td/form/table[1]/tbody/tr[1]/td/table/tbody/tr[7]/td/table/tbody/tr[2]/td[2]");
                 if (regNameNode != null)
                 {
-                    entry.agentfirstname = HttpUtility.HtmlDecode(regNameNode.InnerText);
+                    entry.agentCompany = HttpUtility.HtmlDecode(regNameNode.InnerText);
                 }
 
                 var mailingNode = doc.DocumentNode.SelectSingleNode("//*[@id=\"application\"]/table/tbody/tr/td[2]/table/tbody/tr[3]/td/form/table[1]/tbody/tr[1]/td/table/tbody/tr[7]/td/table/tbody/tr[4]/td[2]");
@@ -85,7 +90,7 @@ async Task FetchRegisteredAgentDetails(List<ColoradoDataset> data)
                               entry.  agentMailingCity = parts[1];
                                  stateZip = parts[2].Trim();
                                 entry.agentMailingState = stateZip.Substring(0, 2);
-                                entry.agentMailingZipcode = stateZip.Substring(2);
+                                entry.agentMailingZip = stateZip.Substring(2);
                                 entry.agentMailingCountry = parts[3];
                                 break;
                             case 5:
@@ -93,8 +98,8 @@ async Task FetchRegisteredAgentDetails(List<ColoradoDataset> data)
                                 entry.agentMailingCity = parts[2];
                                  stateZip = parts[3].Trim();
                                 entry.agentMailingState = stateZip.Substring(0, 2);
-                                entry.agentMailingZipcode = stateZip.Substring(2);
-                                entry.agentMailingCountry = parts[3];
+                                entry.agentMailingZip = stateZip.Substring(2);
+                                entry.agentMailingCountry = parts[4];
                                 break;
                             default:
                                 break;
@@ -106,10 +111,10 @@ async Task FetchRegisteredAgentDetails(List<ColoradoDataset> data)
                     }
                 }
 
-                if (!string.IsNullOrEmpty(entry.agentfirstname))
+                if (!string.IsNullOrEmpty(entry.agentCompany))
                 {
                     string sql = @"UPDATE       ColoradoDatasets
-SET                agentMailingFullAddress =@agentMailingFullAddress, agentMailingAddress =@agentMailingAddress, agentMailingCity =@agentMailingCity, agentMailingState =@agentMailingState, agentMailingZip =@agentMailingZip, agentMailingCountry =@agentMailingCountry, agentfirstname =@agentfirstname
+SET                agentMailingFullAddress =@agentMailingFullAddress, agentMailingAddress =@agentMailingAddress, agentMailingCity =@agentMailingCity, agentMailingState =@agentMailingState, agentMailingZip =@agentMailingZip, agentMailingCountry =@agentMailingCountry, agentCompany =@agentCompany
 where Id=@Id";
                     connection.Execute(sql, entry);
                     Console.WriteLine("Updated => " + entry.entityid);
@@ -139,6 +144,7 @@ public class ColoradoDataset
     public string entitystatus { get; set; }
     public string jurisdictonofformation { get; set; }
     public string entitytype { get; set; }
+    public string agentCompany { get; set; }
     public string agentfirstname { get; set; }
     public string agentlastname { get; set; }
     public string agentprincipaladdress1 { get; set; }
@@ -151,7 +157,7 @@ public class ColoradoDataset
     public string agentMailingAddress { get; set; }
     public string agentMailingCity { get; set; }
     public string agentMailingState { get; set; }
-    public string agentMailingZipcode { get; set; }
+    public string agentMailingZip { get; set; }
     public string agentMailingCountry { get; set; }
 
     public DateTime entityformdate { get; set; }
